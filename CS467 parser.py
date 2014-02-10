081802d0906cefbd456e915a2e1ef854c994364d
azianmike <superman327>
2014-02-09 23:43:32 -0600

    cleaned up, added comments

--- CS467 parser.py
+++ CS467 parser.py
@@ -8,7 +8,5 @@ client = MongoClient('ec2-54-84-86-116.compute-1.amazonaws.com', 27017)
 db = client['enron']
 collections=db.messages  #accesses the enron "messages"
 posts = collections.find_one()  #finds the first message
-print posts
-#print posts['body']
-normalized_body=unicodedata.normalize('NFKD', posts['body']).encode('ascii','ignore')
-word_frequency(normalized_body) ## #
+normalized_body=unicodedata.normalize('NFKD', posts['body']).encode('ascii','ignore')  #converts from unicode to string
+word_frequency(normalized_body)  #pass in string to word_frequency to count
--- word_frequency.py
+++ word_frequency.py
@@ -7,12 +7,6 @@ from operator import itemgetter
 def word_frequency(body):
     out = body.translate(string.maketrans("", ""), string.punctuation)
     body_split = out.split()
-    c=Counter(x.strip() for x in body_split)
-    #for word in body_split:
-        # do something useful with word
-        #print word
-        #d[word]
-    #sorted(c.items())
-    print c #
+    c=Counter(x.strip() for x in body_split)  #counter for each word
     for y in c.most_common():  ##sorts it by most common
         print y

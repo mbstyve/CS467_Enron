fc0f41a1e791c0e64f0b0b3280c1b595d85dfe33
azianmike <superman327>
2014-02-10 18:31:25 -0600

    got JSON dumps working

--- json_encoder.py
+++ json_encoder.py
@@ -2,7 +2,12 @@ __author__ = 'Michael'
 
 import json
 
-dump={'total':{'scrimmage':2, 'other_test':3}}
-test=json.dumps(dump)
-print test
-print dump['total']['scrimmage']
+def dumpJSON(date, dataJSON):
+    dump={'total':{'scrimmage':2, 'other_test':3}}
+    test=json.dumps(dump)
+    print test
+    print dump['total']['scrimmage']
+    with open('JSON dump/Tue, 14 Nov 2000.txt', 'w') as outfile:
+        json.dumps(dump)
+
+dumpJSON('','')

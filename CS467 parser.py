56b1e659c1e94d30f6674bd27793a687d3de5078
azianmike <superman327>
2014-02-10 18:07:04 -0600

    adding in parser and json encoder

--- CS467 parser.py
+++ CS467 parser.py
@@ -2,6 +2,7 @@ __author__ = 'Michael'
 
 from pymongo import MongoClient
 from word_frequency import word_frequency
+from word_frequency import dateParser
 import unicodedata
 
 client = MongoClient('ec2-54-84-86-116.compute-1.amazonaws.com', 27017)
@@ -10,3 +11,9 @@ collections=db.messages  #accesses the enron "messages"
 posts = collections.find_one()  #finds the first message
 normalized_body=unicodedata.normalize('NFKD', posts['body']).encode('ascii','ignore')  #converts from unicode to string
 word_frequency(normalized_body)  #pass in string to word_frequency to count
+posts2 = collections.find()  #should get ALL emails in the database
+print posts2.count()
+print posts2[4000]
+print posts2[1]['headers']['Date']
+date = dateParser(posts2[1])
+print date
--- word_frequency.py
+++ word_frequency.py
@@ -5,8 +5,15 @@ from collections import Counter
 from operator import itemgetter
 
 def word_frequency(body):
-    out = body.translate(string.maketrans("", ""), string.punctuation)
-    body_split = out.split()
+    out = body.translate(string.maketrans("", ""), string.punctuation)  ##gets rid of extraneous punctuation
+    body_split = out.split()  ##splits up the paragraph by each individual word
     c=Counter(x.strip() for x in body_split)  #counter for each word
     for y in c.most_common():  ##sorts it by most common
         print y
+
+
+def dateParser(JSONobject):
+    indexOf200 = JSONobject['headers']['Date'].index('200')
+    onlyDate = JSONobject['headers']['Date'][:indexOf200+4]
+    return onlyDate
+
